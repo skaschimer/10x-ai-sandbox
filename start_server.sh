0322e6406b5034425ea62b616feca8f0dc34af39
jimmoffet <jim.moffet@gsa.gov>
2024-09-10 16:14:41 -0700

    test deploy

--- start_server.sh
+++ start_server.sh
@@ -4,9 +4,9 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 cd "$SCRIPT_DIR" || exit
 
 if [ -n "$VCAP_APPLICATION" ]; then
-    echo "Running inside a Cloud Foundry instance, setting paths"
-    export PATH="$HOME/deps/1/node/bin:$HOME/deps/0/bin:$PATH"
-    export LD_LIBRARY_PATH="$HOME/deps/0/lib:$LD_LIBRARY_PATH"
+    # echo "Running inside a Cloud Foundry instance, setting paths"
+    # export PATH="$HOME/deps/1/node/bin:$HOME/deps/0/bin:$PATH"
+    # export LD_LIBRARY_PATH="$HOME/deps/0/lib:$LD_LIBRARY_PATH"
     echo "PATH is set to: $PATH"
     echo "LD_LIBRARY_PATH is set to: $LD_LIBRARY_PATH"
 
