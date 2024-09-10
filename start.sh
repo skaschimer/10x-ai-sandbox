7ecb3932eeb1bf9225ef36d0c7acf6471d140cda
jimmoffet <jim.moffet@gsa.gov>
2024-09-09 20:49:55 -0700

    test deploy

--- start.sh
+++ start.sh
@@ -6,11 +6,8 @@ cd "$SCRIPT_DIR" || exit
 # Check if the environment variable VCAP_APPLICATION exists
 if [ -n "$VCAP_APPLICATION" ]; then
     echo "Running inside a Cloud Foundry instance, setting paths"
-    # Add Node.js and Python paths
     export PATH="$HOME/deps/1/node/bin:$HOME/deps/0/bin:$PATH"
-    # Set the library path for Python
     export LD_LIBRARY_PATH="$HOME/deps/0/lib:$LD_LIBRARY_PATH"
-    # Echo the paths to verify when you SSH in
     echo "PATH is set to: $PATH"
     echo "LD_LIBRARY_PATH is set to: $LD_LIBRARY_PATH"
 else
