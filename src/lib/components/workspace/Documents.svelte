2efb4a39e0d77003070c8f489f0223ebacd0baa1
jimmoffet <jim.moffet@gsa.gov>
2024-11-06 21:41:21 -0800

    stringify table names and sleep on scans

--- src/lib/components/workspace/Documents.svelte
+++ src/lib/components/workspace/Documents.svelte
@@ -463,26 +463,26 @@
 				</button>
 
 				<!-- <button
-            class="self-center w-fit text-sm px-2 py-2 border dark:border-gray-600 rounded-xl"
-            type="button"
-            on:click={() => {
-                console.log('download file');
-            }}
-        >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                class="w-4 h-4"
-            >
-                <path
-                    d="M8.75 2.75a.75.75 0 0 0-1.5 0v5.69L5.03 6.22a.75.75 0 0 0-1.06 1.06l3.5 3.5a.75.75 0 0 0 1.06 0l3.5-3.5a.75.75 0 0 0-1.06-1.06L8.75 8.44V2.75Z"
-                />
-                <path
-                    d="M3.5 9.75a.75.75 0 0 0-1.5 0v1.5A2.75 2.75 0 0 0 4.75 14h6.5A2.75 2.75 0 0 0 14 11.25v-1.5a.75.75 0 0 0-1.5 0v1.5c0 .69-.56 1.25-1.25 1.25h-6.5c-.69 0-1.25-.56-1.25-1.25v-1.5Z"
-                />
-            </svg>
-        </button> -->
+					class="self-center w-fit text-sm px-2 py-2 border dark:border-gray-600 rounded-xl"
+					type="button"
+					on:click={() => {
+						console.log('download file');
+					}}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 16 16"
+						fill="currentColor"
+						class="w-4 h-4"
+					>
+						<path
+							d="M8.75 2.75a.75.75 0 0 0-1.5 0v5.69L5.03 6.22a.75.75 0 0 0-1.06 1.06l3.5 3.5a.75.75 0 0 0 1.06 0l3.5-3.5a.75.75 0 0 0-1.06-1.06L8.75 8.44V2.75Z"
+						/>
+						<path
+							d="M3.5 9.75a.75.75 0 0 0-1.5 0v1.5A2.75 2.75 0 0 0 4.75 14h6.5A2.75 2.75 0 0 0 14 11.25v-1.5a.75.75 0 0 0-1.5 0v1.5c0 .69-.56 1.25-1.25 1.25h-6.5c-.69 0-1.25-.56-1.25-1.25v-1.5Z"
+						/>
+					</svg>
+				</button> -->
 
 				<button
 					class="self-center w-fit text-sm px-2 py-2 dark:text-gray-300 dark:hover:text-white hover:bg-black/5 dark:hover:bg-white/5 rounded-xl"
