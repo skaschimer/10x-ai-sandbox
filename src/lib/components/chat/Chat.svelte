324caffe89d8b6d210e784e54ce48ccebe6543b0
ethangardner <ethan.gardner@gsa.gov>
2025-02-05 17:17:38 -0500

    provide means of adding a main content region to the page

--- src/lib/components/chat/Chat.svelte
+++ src/lib/components/chat/Chat.svelte
@@ -366,6 +366,8 @@
 		}
 	};
 
+	export let mainContentId;
+
 	onMount(async () => {
 		console.log('mounted');
 		window.addEventListener('message', onMessageHandler);
@@ -1895,7 +1897,7 @@
 					</div>
 				{/if}
 
-				<div class="flex flex-col flex-auto z-10 w-full">
+				<div class="flex flex-col flex-auto z-10 w-full" id={mainContentId ?? undefined}>
 					{#if $settings?.landingPageMode === 'chat' || createMessagesList(history.currentId).length > 0}
 						<div
 							class=" pb-2.5 flex flex-col justify-between w-full flex-auto overflow-auto h-0 max-w-full z-10 scrollbar-hidden"
