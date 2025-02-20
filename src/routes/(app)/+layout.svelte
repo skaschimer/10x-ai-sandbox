dff1f9c1a2e26aab65fb3af274ac779666be151a
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2025-02-20 10:56:36 -0700

    formatting

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -180,7 +180,10 @@
 				}
 			});
 
-			if ($user.role === 'admin' && ($settings?.showChangelog ?? $config?.features.default_show_changelog ?? true)) {
+			if (
+				$user.role === 'admin' &&
+				($settings?.showChangelog ?? $config?.features.default_show_changelog ?? true)
+			) {
 				showChangelog.set($settings?.version !== $config.version);
 			}
 
