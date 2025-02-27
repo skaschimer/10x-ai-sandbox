9d1ac8d69c8218203a35cbdc306096f41ddd64e2
weiwang-gsa <wei.wang@gsa.gov>
2025-02-27 09:47:20 -0500

    added disclaimer

--- backend/open_webui/main.py
+++ backend/open_webui/main.py
@@ -222,6 +222,7 @@ from open_webui.config import (
     ENABLE_ADMIN_FEEDBACKS,
     ENABLE_RECORD_VOICE_AND_CALL,
     ENABLE_MORE_INPUTS,
+    ENABLE_DISCLAIMER,
     # WebUI (OAuth)
     ENABLE_OAUTH_ROLE_MANAGEMENT,
     OAUTH_ROLES_CLAIM,
@@ -1011,6 +1012,7 @@ async def get_app_config(request: Request):
                     "enable_admin_feedbacks": ENABLE_ADMIN_FEEDBACKS,
                     "enable_record_voice_and_call": ENABLE_RECORD_VOICE_AND_CALL,
                     "enable_more_inputs": ENABLE_MORE_INPUTS,
+                    "enable_disclaimer": ENABLE_DISCLAIMER,
                 }
                 if user is not None
                 else {}
