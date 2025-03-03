94489b2cb0d37c4000cc418da7c5f5dd8f45fd8f
weiwang-gsa <wei.wang@gsa.gov>
2025-03-03 16:09:49 -0500

    added feature flag to show and hide ask and explain floating buttons

--- backend/open_webui/main.py
+++ backend/open_webui/main.py
@@ -223,6 +223,7 @@ from open_webui.config import (
     ENABLE_RECORD_VOICE_AND_CALL,
     ENABLE_MORE_INPUTS,
     ENABLE_DISCLAIMER,
+    ENABLE_FLOATING_BUTTONS,
     # WebUI (OAuth)
     ENABLE_OAUTH_ROLE_MANAGEMENT,
     OAUTH_ROLES_CLAIM,
@@ -1013,6 +1014,7 @@ async def get_app_config(request: Request):
                     "enable_record_voice_and_call": ENABLE_RECORD_VOICE_AND_CALL,
                     "enable_more_inputs": ENABLE_MORE_INPUTS,
                     "enable_disclaimer": ENABLE_DISCLAIMER,
+                    "enable_floating_buttons": ENABLE_FLOATING_BUTTONS,
                 }
                 if user is not None
                 else {}
