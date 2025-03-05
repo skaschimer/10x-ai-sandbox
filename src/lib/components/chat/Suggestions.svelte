49cbc79f2046a1e3313487835a820adbe7672368
weiwang-gsa <wei.wang@gsa.gov>
2025-03-05 16:19:22 -0500

    adjusted button space

--- src/lib/components/chat/Suggestions.svelte
+++ src/lib/components/chat/Suggestions.svelte
@@ -35,7 +35,7 @@
 			<div>
 				{#if prompt.title && prompt.title[0] !== ''}
 					<div
-						class="px-3 py-1 border border-gray-400 text-sm font-medium items-center text-gray-400 dark:text-gray-600 bg-white font-semibold rounded-full hover:text-gray-800 hover:border-gray-800"
+						class="px-2 py-2 border border-gray-400 text-sm font-medium items-center text-gray-400 dark:text-gray-600 bg-white font-semibold rounded-full hover:text-gray-800 hover:border-gray-800"
 					>
 						{prompt.title[0]}
 						{prompt.title[1]}
