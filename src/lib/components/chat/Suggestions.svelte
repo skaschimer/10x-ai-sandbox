c6158e75a5bfcd685307361997aef344b7205e54
Robert Jolly <robert.jolly@gsa.gov>
2025-03-13 12:33:34 -0600

    fix suggested prompt font rendering in safari (#396) (#397)

--- src/lib/components/chat/Suggestions.svelte
+++ src/lib/components/chat/Suggestions.svelte
@@ -35,7 +35,7 @@
 			<div>
 				{#if prompt.title && prompt.title[0] !== ''}
 					<div
-						class="px-2 py-2 border border-gray-400 text-sm font-medium items-center text-gray-400 dark:text-gray-600 bg-transparent font-semibold rounded-full hover:text-gray-800 hover:border-gray-800 dark:text-gray-300 dark:border-gray-600 dark:hover:border-gray-100 dark:hover:text-gray-100"
+						class="px-2 py-2 border border-gray-400 text-sm items-center text-gray-400 dark:text-gray-600 bg-transparent rounded-full hover:text-gray-800 hover:border-gray-800 dark:text-gray-300 dark:border-gray-600 dark:hover:border-gray-100 dark:hover:text-gray-100"
 					>
 						{prompt.title[0]}
 					</div>
