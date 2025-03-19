64edde3b1e3927d68ea95396828b3a417831c9df
Robert Jolly <robert.jolly@gsa.gov>
2025-03-19 16:54:37 -0600

    Remove background color from dark and light chat-input (#443)
    Co-authored-by: Eleni Chappen <eleni.chappen@gsa.gov>

--- src/lib/components/chat/MessageInput.svelte
+++ src/lib/components/chat/MessageInput.svelte
@@ -539,7 +539,7 @@
 							}}
 						>
 							<div
-								class="flex-1 flex flex-col border border-gray-400 relative w-full rounded-3xl px-1 bg-gray-600/5 dark:bg-gray-400/5 dark:text-gray-100"
+								class="flex-1 flex flex-col border border-gray-400 relative w-full rounded-3xl px-1 dark:text-gray-100"
 								dir={$settings?.chatDirection ?? 'LTR'}
 							>
 								{#if files.length > 0}
