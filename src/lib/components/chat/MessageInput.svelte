a0b8010b3d243c44424c9d3e11ae86c0235c1707
Jeff Fredrickson <jeff@ejeff.org>
2025-05-22 12:40:02 -0600

    hide svg contents from screen readers

--- src/lib/components/chat/MessageInput.svelte
+++ src/lib/components/chat/MessageInput.svelte
@@ -600,6 +600,7 @@
 															}}
 														>
 															<svg
+																aria-hidden="true"
 																xmlns="http://www.w3.org/2000/svg"
 																viewBox="0 0 20 20"
 																fill="currentColor"
